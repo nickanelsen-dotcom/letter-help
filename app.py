5a65bd0023108741a4b132f4dc43057a943475c0
nickanelsen-dotcom <nickanelsen@gmail.com>
2026-07-14 03:51:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-import os
 
 app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
 def read_root():
-    try:
-        # Tries to safely read the file
-        with open("index.html", "r", encoding="utf-8") as file:
-            return file.read()
-    except Exception as e:
-        # If it fails, it will display the exact error message on the screen
-        return f"<h1>Server Error</h1><p>Details: {str(e)}</p><p>Current Folder Contents: {str(os.listdir('.'))}</p>"
+    with open("index.html", "r", encoding="utf-8") as file:
+        return file.read()
